977a06c6b7faa01a1c0593d6f95bd716e05701b4
matiargs <matias.micheltorena@gmail.com>
2015-10-10 04:15:26 +0000

    Refactor de funciones completa

--- Trenes_Bot.py
+++ Trenes_Bot.py
@@ -11,52 +11,62 @@ API_URL = 'http://trenes.mininterior.gov.ar/apps/api_tiempos_temp.php' # URL del
 
 bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
  
-def listener(messages): # Con esto, estamos definiendo una función llamada 'listener', que recibe como parámetro un dato llamado 'messages'.
-    for m in messages: # Por cada dato 'm' en el dato 'messages'
-        cid = m.chat.id # Almacenaremos el ID de la conversación.
-        print "[" + str(cid) + "]: " + m.text # Y haremos que imprima algo parecido a esto -> [52033876]: /start
-        
-            
-        if m.text == "/help":
-            response = "El tren es tuyo cuidalo. www.trenesargentinos.gob.ar"
-            bot.send_message(cid, response)
-            
-        if m.text == "/sarmiento":
-            paramsRequest = {'ramal': '1'}
-            data = requests.get(API_URL, params=paramsRequest).json()
-            response = "Sale a las: " + data['salidas'][0]['min'] + " Estado: " + data['salidas'][0]['estado'] 
-            bot.send_message(cid, response)
-        
-        if m.text == "/mitre":
-            paramsRequest = {'ramal': '5'}
-            data = requests.get(API_URL, params=paramsRequest).json()
-            response = "Sale a las: " + data['salidas'][0]['min'] + " Estado: " + data['salidas'][0]['estado'] 
-            bot.send_message(cid, response)
-        
-        if m.text == "/roca":
-            response = "Fuera de Servicio por obras de electrificación. Estamos mejorando el servicio para vos :)"
-            bot.send_message(cid, response)
-         
-        if m.text == "/sanmartin":
-            paramsRequest = {'ramal': '31'}
-            data = requests.get(API_URL, params=paramsRequest).json()
-            response = "Sale a las: " + data['salidas'][0]['min'] + " Estado: " + data['salidas'][0]['estado'] 
-            bot.send_message(cid, response)
-        
-        if m.text == "/trendelacosta":
-            paramsRequest = {'ramal': '41'}
-            data = requests.get(API_URL, params=paramsRequest).json()
-            response =  "Sale a las: " + data['salidas'][0]['min'] + " Estado: " + data['salidas'][0]['estado'] 
-            bot.send_message(cid, response)
-        
-        if m.text == "/tarifaRosario":
-            photo = open('horarios.jpg', 'rb')
-            bot.send_message(cid, "Esta es la tarifa Buenos Aires - Rosario")
-            bot.send_photo(cid, photo)
-            
-        if m.text != "/trendelacosta" and  m.text != "/tarifaRosario" and m.text != "/sanmartin" and  m.text != "/roca" and  m.text != "/mitre" and  m.text != "/sarmiento" and  m.text != "/start" and m.text != "/about" and m.text != "/help":
-            bot.send_message(cid, "El comando no es valido :( . Podes ingresar /trendelacosta, /sanmartin, /roca, /mitre, /sarmiento, /buenosairesrosarioTarifa")
+@bot.message_handler(commands=['start'])
+def start(m):
+    bot.send_message(m.chat.id, "Hola, en que te puedo ayudar?") 
  
-bot.set_update_listener(listener) # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba.
+@bot.message_handler(commands=['sarmiento'])
+def sarmiento(m):
+    paramsRequest = {'ramal': '1'}
+    data = requests.get(API_URL, params=paramsRequest).json()
+    response = "Sale a las: " + data['salidas'][0]['min'] + " Estado: " + data['salidas'][0]['estado'] 
+    bot.send_message(m.chat.id, response)
+
+@bot.message_handler(commands=['mitre'])
+def mitre(m):
+    paramsRequest = {'ramal': '5'}
+    data = requests.get(API_URL, params=paramsRequest).json()
+    response = "Sale a las: " + data['salidas'][0]['min'] + " Estado: " + data['salidas'][0]['estado'] 
+    bot.send_message(m.chat.id, response)
+
+@bot.message_handler(commands=['sanmartin'])
+def sanmartin(m):
+    paramsRequest = {'ramal': '31'}
+    data = requests.get(API_URL, params=paramsRequest).json()
+    response = "Sale a las: " + data['salidas'][0]['min'] + " Estado: " + data['salidas'][0]['estado'] 
+    bot.send_message(m.chat.id, response)
+ 
+@bot.message_handler(commands=['trendelacosta'])
+def trendelacosta(m):
+    paramsRequest = {'ramal': '41'}
+    data = requests.get(API_URL, params=paramsRequest).json()
+    response = "Sale a las: " + data['salidas'][0]['min'] + " Estado: " + data['salidas'][0]['estado'] 
+    bot.send_message(m.chat.id, response)
+
+@bot.message_handler(commands=['roca'])
+def roca(m):
+    response = "Fuera de Servicio por obras de electrificación. Estamos mejorando el servicio para vos :)" 
+    bot.send_message(m.chat.id, response) 
+    
+@bot.message_handler(commands=['tarifaRosario'])
+def tarifaRosario(m):
+    photo = open('horarios.jpg', 'rb')
+    bot.send_message(m.chat.id, "Esta es la tarifa Buenos Aires - Rosario")
+    bot.send_photo(m.chat.id, photo)
+
+    
+# def listener(messages): # Con esto, estamos definiendo una función llamada 'listener', que recibe como parámetro un dato llamado 'messages'.
+#     for m in messages: # Por cada dato 'm' en el dato 'messages'
+#         cid = m.chat.id # Almacenaremos el ID de la conversación.
+#         print "[" + str(cid) + "]: " + m.text # Y haremos que imprima algo parecido a esto -> [52033876]: /start
+     
+        
+#         if m.text == "/tarifaRosario":
+            
+            
+#         if m.text != "/trendelacosta" and  m.text != "/tarifaRosario" and m.text != "/sanmartin" and  m.text != "/roca" and  m.text != "/mitre" and  m.text != "/sarmiento" and  m.text != "/start" and m.text != "/about" and m.text != "/help":
+#             bot.send_message(cid, "El comando no es valido :( . Podes ingresar /trendelacosta, /sanmartin, /roca, /mitre, /sarmiento, /buenosairesrosarioTarifa")
+ 
+# bot.set_update_listener(listener) # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba.
  
 bot.polling(none_stop=True) # Con esto, le decimos al bot que siga funcionando incluso si encuentra algún fallo.
